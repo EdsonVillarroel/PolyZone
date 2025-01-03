acd3f72ddec245731a4a04b3499b38d8f28ac45f
Edson Villarroel <villarroel.dev@gmail.com>
2025-01-02 22:17:10 -0400

    add component example

--- main-zone/next.config.ts
+++ main-zone/next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+const { BLOG_URL } = process.env;
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: "/blog/:path+",
+        destination: `${BLOG_URL}/blog/:path+`,
+      },
+      {
+        source: "/blog-static/_next/:path+",
+        destination: `${BLOG_URL}/blog-static/_next/:path+`,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
