b8ce77438e2e86d09ee12782f799440e5c380ee3
Edson Villarroel <villarroel.dev@gmail.com>
2025-01-04 20:24:50 -0400

    active multi zone

--- main-zone/src/app/about/page.tsx
+++ main-zone/src/app/about/page.tsx
@@ -1,9 +1,19 @@
+'use client'
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function About() {
+  useEffect(() => {
+    const channel = new BroadcastChannel("shared-data-channel");
+    
+    channel.postMessage({ key: "theme", value: "dark" });
+  }, [])
+  
+
   return (
     <div>
-      <p>This is the about page.</p>
+      <p>This is the about page. Main</p>
+      
       <div>
         <Link href="/">Go Back</Link>
       </div>
--- sub-zone-1/app/blog/page.tsx
+++ sub-zone-1/app/blog/page.tsx
@@ -1,12 +1,32 @@
+'use client'
 import Link from "next/link";
+import { useEffect } from "react";
 
-export default function About() {
+export default function Blog() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const channel = new BroadcastChannel("shared-data-channel");
+
+      channel.onmessage = (event) => {
+        console.log("Mensaje recibido:", event.data); // { key: 'theme', value: 'dark' }
+      };
+
+      return () => channel.close(); // Cerrar el canal para evitar fugas de memoria
+    }
+  }, []);
+
   return (
     <div>
-      <p>This is the about page.</p>
-      <div>
-        <Link href="/">Go Back</Link>
-      </div>
+      <h3>This is our blog</h3>
+      <ul>
+        <li>
+          <Link href="/blog/post/1">Post 1</Link>
+        </li>
+        <li>
+          <Link href="/blog/post/2">Post 2</Link>
+        </li>
+      </ul>
+      <Link href="/">Home</Link>
     </div>
   );
 }
